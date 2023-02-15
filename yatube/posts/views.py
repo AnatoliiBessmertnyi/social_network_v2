0f3c713462cec6fbae3cbef65031b18decb8ea18
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-15 11:09:01 +0400

    Исправлено по flake8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 # Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
 
+
 def index(request):
-    # Одна строка вместо тысячи слов на SQL: 
+    # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
     # отсортированных по полю pub_date по убыванию
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -17,6 +18,7 @@ def index(request):
     }
     return render(request, 'posts/index.html', context)
 
+
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
@@ -29,4 +31,4 @@ def group_posts(request, slug):
         'text': text,
         'title': title,
     }
-    return render(request, 'posts/group_list.html', context)  
+    return render(request, 'posts/group_list.html', context)
