f5e34836f08a0b4b9896252a3a644a835ea19c08
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-16 14:45:48 +0400

    Исправлены title, обраны лишние комментарии

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -4,19 +4,13 @@ from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Добавляем настройку, которая позволить изменять поле group в любом посте
     list_editable = ('group',)
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 admin.site.register(Post, PostAdmin)
 
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-# Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию
     posts = Post.objects.order_by('-pub_date')[:10]
-    title = 'Последние обновления на сайте'
-    text = 'Это главная страница проекта Yatube'
-    # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
-        'title': title,
-        'text': text,
+
     }
     return render(request, 'posts/index.html', context)
 
@@ -22,13 +14,8 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    text = f'Посты группы "{group.title}"'
-    # Получаем уникальное название 'title'
-    title = f'Записи сообщества "{group.title}"'
     context = {
         'group': group,
         'posts': posts,
-        'text': text,
-        'title': title,
     }
     return render(request, 'posts/group_list.html', context)
