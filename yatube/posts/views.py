0d5c10b70aa5b82ec0a709f0733183df04e42d54
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-23 16:09:42 +0400

    добавлен пагинатор

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.core.paginator import Paginator
 
 posts_per_page = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:posts_per_page]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, posts_per_page) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:posts_per_page]
+    posts = group.posts.all()
+    paginator = Paginator(posts, posts_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
