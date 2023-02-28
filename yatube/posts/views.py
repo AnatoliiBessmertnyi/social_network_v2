04fccf972b4fac69aebc83a645a27c1bf859ac20
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-28 17:43:05 +0400

    сделана и настроена страница profile и post_detail

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 from django.core.paginator import Paginator
 
 posts_per_page = 10
@@ -28,3 +28,25 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author).order_by('-pub_date')
+    paginator = Paginator(posts, posts_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/post_detail.html', context)
